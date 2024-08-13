9d5f9b74c1e174b8e998810d0339bc6c1238be4b
Godson Jerry <jegoisgs66@gmail.com>
2024-08-13 11:07:32 +0100

    trailing commas

--- united_sky_trust/settings.py
+++ united_sky_trust/settings.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    
+    "corsheaders",
 
     'rest_framework',
     'authentication',
