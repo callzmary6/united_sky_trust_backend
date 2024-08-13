4839e9de080c254208aa345443839fbaeef05378
Godson Jerry <jegoisgs66@gmail.com>
2024-08-13 11:43:16 +0100

    added middle name to response for login admin

--- authentication/views.py
+++ authentication/views.py
@@ -81,6 +81,7 @@ class LoginAccountManager(generics.GenericAPIView):
             'users': {
                 'email': user['email'],
                 'first_name': user['first_name'],
+                'middle_name': user['middle_name'],
                 'last_name': user['last_name']
             }
         }
