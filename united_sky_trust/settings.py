5716c48a82755e3a90f80558e20beabdb5d45ab4
Godson Jerry <jegoisgs66@gmail.com>
2024-08-13 11:21:38 +0100

    django-cors-headers installed
    updated the requirements.txt file for cors headers

--- united_sky_trust/settings.py
+++ united_sky_trust/settings.py
@@ -32,8 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    "corsheaders",
+    'corsheaders',
 
     'rest_framework',
     'authentication',
